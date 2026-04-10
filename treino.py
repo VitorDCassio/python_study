b85feb4d457c81f06a86c396a5be75c42f1a30d2
Vitor D. Cassio <vitor.cassio@havir.local>
2026-04-10 12:52:56 -0300

    Treinando Funções

--- treino.py
+++ treino.py
@@ -1,26 +1,6 @@
-## 1. Encruzilhadas Lógicas (if / elif / else)
+def calcular_tesouro(saque_navio1,saque_navio2,):
+    saque_total = saque_navio1 + saque_navio2
+    return saque_total
 
-#No Python, o controle de fluxo é ditado pela indentação (os espaços antes do código). 
-# Não usamos chaves {}. Se a condição for verdadeira (True), o bloco indentado abaixo 
-# dela é executado. O Python avalia de cima para baixo. Assim que ele encontra uma 
-# condição verdadeira ele executa aquele bloco e ignora todo o resto.
-
-## 2. Os Domadores de Repetição (for e while)
-
-#O FOR não é aquele contador clássico do C ou Java (for i=0; i<10; i++). Ele é um 
-# iterador. Ele "passeia" por dentro de uma coleção de itens (como uma lista ou um 
-# intervalo de números). Para gerar números rapidamente, usamos o 
-# range(inicio, parada, passo). 
-#O WHILE repete um bloco de código enquanto uma condição for verdadeira. 
-# Cuidado com os loops infinitos!
-
-for i in range(1,11):
-    if i % 2 == 0:
-        print(f"{i} é parSith")
-    else:
-        print(f"{i} é ímpar Jedi")
-
-a = 0
-while a < 3:
-    print(f"While: {a}")
-    a += 1
+total_ouro_jack = calcular_tesouro(850,1250)
+print(f"O Capitão Jack Sparrow roubou um total de {total_ouro_jack} moedas de ouro!")

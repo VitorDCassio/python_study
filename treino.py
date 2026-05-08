7938edd154db5f8affe1ea6c1e7f6fc6818cb092
Vitor D Cassio <vitordcassio@gmail.com>
2026-05-07 23:05:09 -0300

    Treinando lambda

--- treino.py
+++ treino.py
@@ -1,21 +1,8 @@
-# Nossa fila principal (Lista) contendo as fichas dos caminhões (Dicionários)
-patio_havir = [
-    {"placa": "BR-101", "fornecedor": "Celulose Alpha", "status": "Aguardando"},
-    {"placa": "SP-202", "fornecedor": "Bobinas Omega", "status": "Descarregando"}
+radares = [
+    {"placa": "RX-111", "multas": 5},
+    {"placa": "BR-222", "multas": 2},
+    {"placa": "SP-333", "multas": 8}
 ]
 
-print("--- INÍCIO DO TURNO ---")
-print(patio_havir)
-print("\n--- EXECUTANDO OPERAÇÕES ---")
-
-# Escreva os seus códigos do Desafio abaixo desta linha:
-
-patio_havir.append({"placa":"MG-404", "fornecedor":"Embalagens Eco", "status":"Aguardando"})
-
-print(patio_havir[0]["fornecedor"])
-
-patio_havir[0]["status"] = "Descarregando"
-
-patio_havir.pop(1)
-
-print(patio_havir)
+piores_motoristas = sorted(radares, key=lambda x:x["multas"], reverse=True)
+print(piores_motoristas)

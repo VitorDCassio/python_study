a3082d0fb7691392d80a0a30fe3a1fa267c2b31d
Vitor D Cassio <vitordcassio@gmail.com>
2026-06-07 13:47:15 -0300

    LIMPANDO O TREINO.PY

--- treino.py
+++
@@ -1,8 +0,0 @@
-radares = [
-    {"placa": "RX-111", "multas": 5},
-    {"placa": "BR-222", "multas": 2},
-    {"placa": "SP-333", "multas": 8}
-]
-
-piores_motoristas = sorted(radares, key=lambda x:x["multas"], reverse=True)
-print(piores_motoristas)
